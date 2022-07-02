8cf9237bee96834b6adcbece86d533341852898e
umaresso <umarkhatab467@gmail.com>
2022-07-01 19:08:27 -0700

    Added Fibnocii Numbers Generator

---
+++ fib.rs
@@ -0,0 +1,25 @@
+
+pub fn calculate_nth_fib_number(n:u8)->i128{
+    let mut a=0;
+    let mut b=1;
+    let mut c=0;
+    if n<1{
+        return -1;
+    }
+    else if n==1{
+        return a
+    }
+    else if n==2{
+        return b
+    }
+    
+    for i in (2..n){
+        c=a+b;
+        a=b;
+        b=c;
+
+    }
+    
+    return c;
+
+}
--- main.rs
+++ main.rs
@@ -1,5 +1,8 @@
-
+mod fib;
 
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, Rustaceans !");
+    println!("7th Fib Bumber is {}",fib::calculate_nth_fib_number(7)); // should print 8
+    println!("8th Fib Number is {}",fib::calculate_nth_fib_number(8)); // should print 13 as 8+5 is 13
+    
 }
