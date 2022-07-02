a75f56781653c6f1b61af93e0bdf8be946e91c03
umaresso <umarkhatab467@gmail.com>
2022-07-02 09:07:58 -0700

    Added Structs Rectangle Area Struct code

--- react_Area_structs_practice.rs
+++ react_Area_structs_practice.rs
@@ -1,15 +1,33 @@
 
 
+
+struct Rectangle{
+    width:u32,
+    height:u32,
+
+}
 pub fn main(){
 let react_prams=(30,50);  // tuple view of Reactangle Parameters
-println!("\nThe Area of Rectange is := {} \n",area(&react_prams))
+let params = Rectangle{width:30,
+    height:50
+    };
+
+println!("\nThe Area of Rectange using Tuple is := {} \n",area(&react_prams))
+println!("\nThe Area of Rectange using Struct is := {} \n",area_through_struct(&params))
 
 
 }
 
-fn area(react_prams:&(u32,u32) )->u32{
-    return react_prams.0*react_prams.1;
+fn area(rect_prams:&(u32,u32) )->u32{
+    return rect_prams.0*rect_prams.1;
     // unable to detect the meaning of indexed data
+
+}
+
+fn area_through_struct(rect_prams:&Rectangle)->u32{
+    return rect_prams.width*rect_prams.height;
+    
+    // More Meaningful Calculation
 
 }
 
