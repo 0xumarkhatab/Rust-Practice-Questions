7eb9612f5c9c9f0e2ce049d8887a65d95c338e58
umaresso <umarkhatab467@gmail.com>
2022-07-02 02:28:17 -0700

    Added Looping Menu Functionality in database File

--- database_system.rs
+++ database_system.rs
@@ -2,55 +2,64 @@ use crate::Reader::{read_console_line, read_int};
 
 
 pub fn simulate(){
-    println!("\t\t-- Welcome to Rust Interaction Database -- \n");
-    println!("Select the Operation to Perform ! \n");
-    println!("1-> Add Record \n2-> Remove Record \n3-> Print Records\n4->Exit");
+    print!("\n\t\tHapering with Vectors \t\t\n");
+    
     // We are using String here instead of &str because string literals 
     // '&str' are immutable
     
     let mut the_names:Vec<String>=Vec::new();
-    let mut choice=0;
-    choice= read_int();
-    match choice {
-        1=>{
-            println!("\nEnter Your name:= ");
-            let record =read_console_line();
-            the_names = insert_record(record, the_names.clone());
-            println!("\n\t\tRecord Has Been Added Successfully !....\n");
-
-        },
-        2=> {
-            println!("Removing ! ..")
-        },
-        3=>{
-            printDatabase(the_names.clone());
-
-        },
-        4=>{
-            println!("Exitting !")
+    let mut choice:i32 =10;
+    const EXIT_CHOICE:i32 = 4;
+    while choice!=EXIT_CHOICE{
+        println!("\t\t-- Welcome to Rust Interaction Database -- \n");
+        println!("Select the Operation to Perform ! \n");
+        println!("1-> Add Record \n2-> Remove Record \n3-> Print Records\n4->Exit");
+        choice= read_int();
+        match choice {
+            1=>{
+                println!("\nEnter Record name:= ");
+                let record =read_console_line();
+                the_names = insert_record(record, the_names.clone());
+                println!("\n\t\tRecord Has Been Added Successfully !....\n");
+            },
+            2=> {
+                println!("\nRemoving Record  ");
+    
+            },
+            3=>{
+                printDatabase(the_names.clone());
+            },
+            EXIT_CHOICE=>{
+                println!("\n\t\tExitting !... ");
+            }
+            _=>println!("invalid")
+    
+            
         }
-        _=>println!("invalid")
-
-        
+    
     }
+    
+
 
 
 }
+
 fn insert_record(record:String,the_names:Vec<String> )->Vec<String>{
 
-    let mut name=String::from(record); //using the String Constructor
-    // Cloning because of the ownership behaviour of Rust
-    let mut state = the_names.clone();
-    state.push(name.clone()); 
-    
-    return state;
-    
+        let mut name=String::from(record); //using the String Constructor
+        // Cloning because of the ownership behaviour of Rust
+        let mut state = the_names.clone();
+        state.push(name.clone()); 
+        
+        return state;
+        
 }
+
 fn printDatabase(db:Vec<String>){
     println!("\n\n\t\t---- Printing Database State ---- \n");
     let mut index=1;
     for element in db.iter(){
-        println!("{} -> {}\n",index,element);
+        println!("{} -> {}",index,element);
         index=index+1;
     }
 
