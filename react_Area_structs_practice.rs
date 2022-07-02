43e446c7e4f7c6c79e32adcfcaa2cf32eed3cedd
umaresso <umarkhatab467@gmail.com>
2022-07-02 08:55:49 -0700

    Added Structs Rectangle Area Dummy code

---
+++ react_Area_structs_practice.rs
@@ -0,0 +1,15 @@
+
+
+pub fn main(){
+let width = 30; //pixels
+let height=50;  //pixels
+
+println!("\nThe Area of Rectange is := {} \n",area(width,height))
+
+
+}
+
+fn area(width:u32,height:u32){
+    width*height;
+}
+
